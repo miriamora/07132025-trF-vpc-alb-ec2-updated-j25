4508aacdd123e2b472de4aa464c2a6ca6d1c614b
miriamora <miriamoranu@gmail.com>
2025-07-14 11:52:50 -0700

    alb-ec2-using trF - 20250414

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     region       = "us-east-1"
-    bucket       = "testbucketforalb-20250714"
+    bucket       = "**replace_with_your_bucket_name**"
     key          = "alb/terraform.tfstate"
     use_lockfile = true
     encrypt      = true
